ad0a984c39ae81a606b39aef91fba608d4bed178
aaronletbu <112291697+aaronletbu@users.noreply.github.com>
2024-09-23 13:21:04 -0400

    Add files via upload

---
+++ leanclass/assignments/homework_1_template.lean
@@ -0,0 +1,16 @@
+import Mathlib.Data.Real.Basic
+import Library.Basic
+
+math2001_init
+
+/-! # CS 511 Homework 1 -/
+
+/- 6 points -/
+theorem exercise3 {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by sorry
+
+/- 6 points -/
+theorem exercise4 {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 := by sorry
+
+/- 6 points -/
+theorem problem2 {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3)
+    (h3 : c = 1) : a = 2 := by sorry
